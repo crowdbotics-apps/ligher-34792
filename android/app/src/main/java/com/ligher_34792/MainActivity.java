d57115be8c0c2d2c0fb0cdae7eb04abed6ea9525
Crowdbotics <team@crowdbotics.com>
2022-04-17 12:56:04 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/ligher_34792/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ligher_34792;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ligher_34792";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "ligher_34792",
+  url: "https://ligher_34792.botics.co",
+  api: "https://ligher_34792.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
